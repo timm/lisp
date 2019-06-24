f2dc6c815497403e5a90249ffe0a2a94c0fbaaf6
Tim Menzies <tim.menzies@gmail.com>
2019-06-23 21:07:46 -0400

    saving

--- src/got.lisp
+++ src/got.lisp
@@ -3,28 +3,32 @@
    files
    gotten)
   (labels 
-    ((knowns (x) 
-	     (remove-if-not #'(lambda (z) (ends-with-p z x)) files))
+    ((knowns 
+       (x) 
+       (remove-if-not #'(lambda (z) (ends-with-p z x)) files))
 
-     (containsp (str1 str2)
-		(unless (string-equal str2 "")
-		  (loop for p = (search str2 str1) 
-			then (search str2 str1 :start2 (1+ p))
-			while p 
-			collect p)))
-     (ends-with-p (str1 str2)
-		  (let ((p (mismatch str2 str1 :from-end T)))
-		    (or (not p) (= 0 p))))
-     (got1 (f)
-	   (if (ends-with-p f "/") 
-	     (dolist (file (directory (format nil "~a/*.lisp" f)))
-	       (got  (namestring file)))
-	     (progn 
-	       #-sbcl (load f) 
-	       #+sbcl (handler-bind
-			((style-warning #'muffle-warning))
-			(load f)))))
-     )
+     (containsp 
+       (str1 str2)
+       (unless (string-equal str2 "")
+	 (loop for p = (search str2 str1) 
+	       then (search str2 str1 :start2 (1+ p))
+	       while p 
+	       collect p)))
+     (ends-with-p 
+       (str1 str2)
+       (let ((p (mismatch str2 str1 :from-end T)))
+	 (or (not p) (= 0 p))))
+     (got1 
+       (f)
+       (format t "~&; loading ~a~%" f)
+       (if (ends-with-p f "/") 
+	 (dolist (f (directory (format nil "~a/*.lisp" f)))
+	   (got (namestring f)))
+	 (progn 
+	   #-sbcl (load f) 
+	   #+sbcl (handler-bind
+		    ((style-warning #'muffle-warning))
+		    (load f))))))
 
     (dolist (pat pats)
       (dolist (file (directory pat))
@@ -39,7 +43,5 @@
 	      (error "ambiguous file name [~a]~%" f)
 	      (unless (member (car where) gotten)
 		(push (car where) gotten)
-		(format t "~&; loading ~a~%" f)
-		(got1 (car where)) ; just right
-	      ))))))))
+		(got1 (car where))))))))))
 
