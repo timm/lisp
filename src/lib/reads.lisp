b86eea6c3581843d93944a2cedbae1d717b23810
Tim Menzies <tim.menzies@gmail.com>
2019-06-24 23:11:03 -0400

    saving

--- src/lib/reads.lisp
+++ src/lib/reads.lisp
@@ -1,17 +1,7 @@
-; vim: ts=2 sw=2 sts=2  et
+; vim: ts=2 sw=2 sts=2 et
 (unless (fboundp 'got) (load "../got"))
 
-(defun string-lines (str)
-  "Convert a string to a list of lines."
-  (labels
-    ((nl    (z)    (char= z #\Newline))
-     (where (pos0) (position-if #'nl str :start pos0))
-     (worker (pos0 &aux pos)
-        (if (setf pos (where pos0))
-            (cons (subseq str pos0 pos)
-                  (worker (1+ pos)))
-            (list (subseq str pos0)))))
-    (worker 0)))
+(got "macros.lisp")
 
 (defun reads (f &key  (act #'print) (get #'read) (str t))
   "Read  a file, calling 'fn' on each s-expression. "
@@ -20,10 +10,15 @@
           while lst do 
           (funcall act lst))))      
 
-(defun para1 (f &aux tmp)
-  (reads f :get #'read-line :act (lambda (s)
-    (if (equalp "" (string-trim '(#\Space #\Tab) s))
-        (return-from 
-           para1 
-          (format nil "~{~a~^~%~}" (reverse tmp)))
-        (push s tmp)))))
+(defun para1 (f)
+  (with-output-to-string (out)
+    (tagbody
+       (reads f :get #'read-line :act (lambda (s)
+         (if (equalp "" (string-trim '(#\Space #\Tab) s))
+             (go exit)
+             (format out "~a~%" s))))
+        exit)))
+
+(defun lines (x &optional (s (make-string-input-stream x)))
+  (aif (read-line s nil)
+       (cons it (lines  s))))
