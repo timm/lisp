a12337553a8753a37f787b6bf626672e6453e99d
Tim Menzies <tim.menzies@gmail.com>
2019-06-27 08:52:52 -0400

    saving

--- src/got.lisp
+++ src/got.lisp
@@ -1,23 +1,17 @@
-; vim: ts=2 sw=2 sts=2  et
+;; vim: ts=2 sw=2 sts=2  et :
+;-------- -------- -------- -------- -------- --------
+(unless (fboundp 'got) (load "../got"))
+
 (let 
-  ((pats '("../*/" "../*/*.lisp"))
+  ((pats '("../*.lisp" "../*/" "../*/*.lisp"))
    files
    gotten)
   (labels 
     ((knowns (x) 
         (remove-if-not #'(lambda (z) (end z x)) files))
-
-     (containsp (s1 s2)
-        (unless (string-equal s2 "")
-          (loop for p = (search s2 s1) 
-                then (search s2 s1 :start2 (1+ p))
-                while p 
-                collect p)))
-
      (end (s1 s2)
         (let ((p (mismatch s2 s1 :from-end T)))
           (or (not p) (= 0 p))))
-
      (got1 (f)
         (format *error-output* "~&; loading ~a~%" f)
         (if (end f "/") 
@@ -43,3 +37,22 @@
               (unless (member (car where) gotten)
                 (push (car where) gotten)
                 (got1 (car where))))))))))
+
+(defmacro getfs (plist first-slot &rest more-slots)
+	"Recursive access to plists; 
+  e.g. `(getfs plist 'a 'b c)` expands to 
+  `(getf (getf (getf plist 'a) 'b) c)`.
+  Built using advice from https://goo.gl/dqnmvH."
+	(if (null more-slots)
+		`(getf ,plist ,first-slot)
+		`(getfs (getf ,plist ,first-slot) ,@more-slots)))
+
+(defmacro my (x &rest y)
+  "Access global config options."
+  `(getfs +config+ ,x ,@y))
+
+(defun config0()
+  '(:rand  (:seed 10013)
+    :col   (:p 2)))
+
+(defparameter +config+ (config0))
--- src/lib/rand.lisp
+++ src/lib/rand.lisp
@@ -1,18 +1,33 @@
 ;; vim: ts=2 sw=2 sts=2 et:
-;-------- -------- -------- -------- -------- -------- --------
+;-------- -------- -------- -------- -------- --------
 (unless (fboundp 'got) (load "../got"))
 
-(let* ((seed0      10013)
-       (seed       seed0)
+(fyi "The LISP random number generator does not let me
+easily set the same seeds on mutilple plaforms. Hemce,
+these 12 lines of code.
+
+`reset-seed (&optional n)`
+
+<ul>Reset seed to `n` (default = `(my :rand :seed)`</ul>
+
+`randf (&optional (n 100))
+
+<ul>Generate a random float in the range 0.. n. </ul>
+
+`randi (&optional (n 1))
+
+<ul>Generate a random integer in the range 0.. n. </ul>")
+
+(let* ((seed       (the :rand :seed))
        (multiplier 16807.0d0)
        (modulus    2147483647.0d0))
-  (defun reset-seed (&optional (n seed0))  
+  (defun reset-seed (&optional (n (the :rand :seed)))  
     (setf seed n))
   (defun randf (&optional (n 1)) 
-    (* n (- 1.0d0 (park-miller-randomizer))))
-  (defun randi (&optional (n 1)) 
+    (* n (- 1.0d0 (_park-miller-randomizer))))
+  (defun randi (&optional (n 100)) 
     (floor (* n (/ (randf 1000.0) 1000))))
-  (defun park-miller-randomizer ()
+  (defun _park-miller-randomizer ()
     "cycle= 2,147,483,646 numbers"
     (setf seed (mod (* multiplier seed) modulus))
     (/ seed modulus)))
