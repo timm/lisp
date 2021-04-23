048108c4188d01a78a40f537db37d272317ba44c
Tim Menzies <timm@ieee.org>
2021-04-23 01:36:51 -0400

    saving

--- espy/etc.lisp
+++ espy/etc.lisp
@@ -9,31 +9,28 @@
   (c (is 2 "c stuff")) 
   (d (is 333 "about d")))
 
-(defun as (s) (or (read-from-string s) s))
+(defun as(s) (or (read-from-string s) s))
 
 (defun cli (b4 &optional (usage ""))
-  (let* (arg
-          (args  (cdr sb-ext:*posix-argv*))
-          (out   (make-instance (type-of b4)))
-          (slots (mapcar #'slot-name1 (klass-slots b4))))
-    (dolist (slot slots)
-      (setf (slot-value out slot) (car (slot-value b4 slot))))
-    (loop while (setf arg (pop args)) do
-      (if (equalp arg "-h")
-        (progn
-          (format t "~&~%~a~%~%USAGE:~%~%" usage)
-          (dolist (slot slots)
-            (let* ((val  (first (slot-value b4 slot)))
-                   (help (second (slot-value b4 slot)))
-                   (pre  (if val "-" "+")))
-              (format t " ~a~(~10a~) ~a (default=~a)~%" pre slot help val))))
-        (let ((pre  (subseq arg 0 1))
-              (flag (intern (string-upcase (subseq arg 1)))))
-          (if (member flag slots)
-              (cond 
-                ((equal pre "-") (setf (slot-value out flag) (as (pop args))))
-                ((equal pre "+") (setf (slot-value out flag) t)))
-              (format t "W> ~(~a~)?~%" arg)))))
-    out))
+  (let* (arg out (args  (cdr sb-ext:*posix-argv*))
+                 (slots (mapcar #'slot-name1 (klass-slots b4))))
+    (labels ((init (s) (dolist (slot slots s)
+                           (setf (slot-value s slot) (car (slot-value b4 slot)))))
+             (help ()  (format t "~&~%~a~%~%USAGE:~%~%" usage)
+                       (dolist (slot slots)
+                         (let ((x (slot-value b4 slot)))
+                           (format t " ~a~(~10a~) ~a (default=~a)~%" 
+                             (if (first x) "-" "+") slot (second x) (first x)))))
+             (update (flag pre)
+                (if (equal pre "-") (setf (slot-value out flag) (as (pop args))))
+                (if (equal pre "+") (setf (slot-value out flag) t))))
+      (setf out (init (make-instance (type-of b4))))
+      (loop while (setf arg (pop args)) do
+            (let* ((pre  (subseq arg 0 1))
+                   (flag (intern (string-upcase (subseq arg 1)))))
+              (cond ((equalp arg "-h")  (help))
+                   ((member flag slots) (update flag pre))
+                   (t                   (format t "W> ~(~a~)?~%" arg))))))
+      out))
 
-(print (cli (make-a) "./lisp etc.lisp" ))
+  (print (cli (make-a) "./lisp etc.lisp" ))
