b081c0e804bd964cb3b3734b5f2d70f24763ff7e
Tim Menzies <timm@ieee.org>
2021-07-01 14:25:00 -0400

    save

--- src/espy/etc.lisp
+++ src/espy/etc.lisp
@@ -1,46 +1,86 @@
 ; vim: noai:ts=2:sw=2:et: 
+"
+## Etc
+
+Misc utils
+"
+
 (defpackage :espy-misc-utils 
   (:export todo)
   (:use :cl)  (:nicknames :etc))
 (in-package :etc)
 
+
 (defun loading (file)
+  "Load a file, ignoring the SBCL messages."
   (format *error-output* "; loading ~(~a~) ...~%" file)
   (handler-bind ((style-warning #'muffle-warning))
     (load file)))
 
-(loading "config"); aa
+(loading "config");
+
+"###  Macros"
 ;-------------------------------------
 (defmacro aif (test yes &optional no)
+  "Anaphoric if (the result of the condition 
+   is cached in `it`)."
+
   `(let ((it ,test)) (if it ,yes ,no)))
 
 (defmacro whale (expr &body body) 
+  "Anaphoric while (the current of the loop controller 
+   is cached in `a`)."
   `(do ((a ,expr ,expr)) ((not a)) ,@body))
 
 (defmacro ? (p x &rest xs)
+  "Recursive  plist accessor; e.g. `(? p :outer :inner)`."
   (if (null xs) `(getf ,p ,x) `(? (getf ,p ,x) ,@xs)))
 
+(defmacro o (s x &rest xs)
+  "Recurse struct accessor; e.g. `(o s address street number)`."
+  (if (null xs) `(slot-value ,s ,x) `(o (slot-value ,s ,x) ,@xs)))
+
 (defmacro want (x &rest y)
+  "Simpler assert statement."
   `(assert ,x () ,@y))
 
-(defun rnd (number &optional (precision 0))
-  (let ((div (expt 10 precision)))
+(defun rnd (number &optional (places 0))
+  "Return  number with `places` number of decimals."
+  (let ((div (expt 10 places)))
     (float (/ (round (* number div)) div))))
 
 ;-------------------------------------
-(let* ((seed0      10013)
-       (seed       seed0)
-       (multiplier 16807.0d0)
-       (modulus    2147483647.0d0))
-  (defun srand (&optional (n seed0))  (setf seed n))
-  (defun randi (&optional (n 1)) 
-    "random integers 0.. n-1"
-    (floor (* n (/ (randf 1000.0) 1000))))
-  (defun randf (&optional (n 1.0)) 
-    (setf seed (mod (* multiplier seed) modulus))
-    (* n (- 1.0d0 (/ seed modulus)))))
+"### Random Numbers
+
+I confess that I never found a way to do
+platform independent random number generation with
+CommonLisp. So I write my own."
+
+(defvar *seed* 10013)
+(defun _park-miller (n)
+  (let ((multiplier 16807.0d0)
+        (modulus    2147483647.0d0))
+    (setf *seed* (mod (* multiplier *seed*) modulus))
+    (* n (- 1.0d0 (/ *seed* modulus)))))
+
+(defun randf (&optional (n 1.0)) 
+  "Return a random flaot 0..n-1."
+  (_park-miller n))
+
+(defun srand (&optional (n 10013))  
+  "Reset random number seed,"
+  (setf *seed* n))
+
+(defun randi (&optional (n 1)) 
+  "Return a random integer 0.. n-1"
+  (floor (* n (/ (randf 1000.0) 1000))))
 
 ; ------------------------------------
+"### System
+
+Wrapper functions to SBCL system functions with strange 
+names."
+
 (defun halt (&optional (status 0)) (sb-ext:exit :code status))
 (defun argv () sb-ext:*posix-argv*)
 
---
+++ src/readme.lisp
@@ -0,0 +1,61 @@
+;; vim: ts=2 sw=2 sts=2 et:
+;-------- -------- -------- -------- -------- -------- --------
+(unless (fboundp 'got) (load "../got"))
+
+"### Usage
+
+```bash
+cd src/xx
+sbcl --script ../lib/readme.lisp > README.md
+git add README.md
+``` "
+
+(defmacro doread ((it f &optional out 
+                      &key (take #'read)) &body body)
+  "Iterator for running over files or strings."
+  (let ((str (gensym)))
+    `(with-open-file (,str f)
+       (loop for 
+             ,it = (handler-case (funcall ,take ,str)
+                   (end-of-file () (loop-finish)))
+             while ,it do 
+             (progn ,@body))
+       ,out)))
+
+(defun args () sb-ext:*posix-argv*)
+
+(defun readme(dir &optional (s t))
+  "Generate README.md from all doco strings 
+  form all LISP code in a directory."
+  (format t "~a~%# ~a~%~%~%" 
+          (para1 "../../README.md")
+          (string-upcase dir))
+  (dolist (f (sort (directory "*.lisp") 
+                   #'(lambda (x y) (string< (pathname-name x) 
+                                            (pathname-name y)))))
+    (let ((name (pathname-name f)))
+      (format t "~%~%## [~a.lisp](~a.lisp)~%~%" name name)
+      (doread (x f)
+        (labels
+          ((defp   () (member (first x) '(defun 
+                                           defmacro defmethod)))
+           (fyip   () (stringp x))
+           (secret () (char= #\_ (elt (symbol-name (second x)) 0)))
+           (docp   () (and    (> (length x) 3)
+                              (stringp (fourth x))
+                              (not (equal "" (fourth x)))))
+           (dump   (str  &optional (pad ""))
+                   (format s "~a~a~%" pad str)))
+          (when (fyip)
+            (format t "~%~%~a~%~%" x)
+            )
+          (when (and (defp) (docp) (not (secret)))
+            (format s "~%`~(~a~) ~(~a~)`~%~%<ul>" 
+                    (second x) (or (third x) ""))
+            (dump (fourth x) "   ")
+            (format s "</ul>~%")))))))
+
+(let ((cli (args)))
+  (if (and cli (equalp "--makedoc" (first cli)))
+    (readme (second cli))))
+  
