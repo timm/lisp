a6e3fe26f195e1410babd4e6988d9c93d4a443d7
Tim Menzies <timm@ieee.org>
2021-08-05 20:37:51 -0400

    save

--- src/tricks.lisp
+++ src/tricks.lisp
@@ -1,20 +1,28 @@
 ; vim: filetype=lisp ts=2 sw=3 sts=2 et :
+
+; ## Macros
+
+; After calling `(reader #\c fun)`, then if
+; ever we see c(sex) then (sexp) will be  
+; passes to `fun`.
 (defmacro reader (com fun)
   (let ((fun1 (gensym)))
-    `(progn (defun ,fun1 (stream char)
-              (declare (ignore char))
-              (,fun (read stream t nil t)))
-            (set-macro-character ,com #',fun1))))
+    `(progn 
+       (defun ,fun1 (stream char)
+         (declare (ignore char))
+         (,fun (read stream t nil t)))
+       (set-macro-character ,com #',fun1))))
 
+; Shorthand for recursive calls to slot-value.
 (defmacro ? (s x &rest xs) 
   (if xs `(? (slot-value ,s ',x) ,@xs) `(slot-value ,s ',x)))
 
+; Anaphoric if.
 (defmacro aif (test yes &optional no) 
-  "anaphoric if"
   `(let ((it ,test)) (if it ,yes ,no)))
 
+; Returns all functions in a package.
 (defun funs (package)
-  "Retrieves all functions in a package."
   (when (find-package package)
     (let ((res (list)))
       (do-all-symbols (sym package)
@@ -24,6 +32,8 @@
           (push sym res)))
       res)))
 
-(defun halt (&optional (status 0)) (sb-ext:exit :code status))
+; Exit LISP.
+(defun halt (&optional (status 0)) 
+  (sb-ext:exit :code status))
 
 
