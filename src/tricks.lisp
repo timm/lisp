962c60c18b1cd98adb345c1b708641a1017cf40c
Tim Menzies <timm@ieee.org>
2021-08-08 21:34:41 -0400

    save

--- src/tricks.lisp
+++ src/tricks.lisp
@@ -85,12 +85,11 @@
         (push s out))))
   out)
 
-(defun timings (function)
-  (let ((real-base (get-internal-real-time))
-        (run-base (get-internal-run-time)))
+(defun timeit (function)
+  (let ((real-base (get-internal-real-time)))
     (funcall function)
-    (values (/ (- (get-internal-real-time) real-base) internal-time-units-per-second)
-            (/ (- (get-internal-run-time) run-base) internal-time-units-per-second))))
+    (/ (- (get-internal-real-time) real-base)
+       internal-time-units-per-second)))
 
 ; Comma-seperated-files
 ; --------------------
