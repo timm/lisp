0431b3b6c7a9834fa6fe4d567c4a1d3bb026e555
Tim Menzies <timm@ieee.org>
2021-08-09 10:03:58 -0400

    save

--- src/rows.lisp
+++ src/rows.lisp
@@ -1,15 +1,17 @@
 ; vim: ts=2 sw=2 et:
 
+; create the  right  kind of column,
+; place it in  the  right kind  of places
 (defun column (txt at rows)
-  (let ((what (if (upper-case-p (subseq txt 0 0)) 'num 'sym))
-        (it   (make-instance what :txt txt :at at)))
-    (if (has txt #\-) 
+  (let* ((what (if (upper-case-p (subseq txt 0 1)) 'num 'sym))
+         (it   (make-instance what :txt txt :at at)))
+    (if (has txt #\-) ; something to minimize
       (setf (? it w -1)))
-    (push it (? rows cols all))
-    (when (not (has txt #\?))
-      (if (has txt #\!) 
+    (push it (? rows cols all)) 
+    (when (not (has txt #\?)); not skipping
+      (if (has txt #\!)  ; klass column
         (setf (? it rows cols klass) it))
-      (if (has txt #\- #\+ #\!)
+      (if (has txt #\- #\+ #\!) ; goal column
         (push it (? rows cols y))
         (push it (? rows cols x))))
     it))
