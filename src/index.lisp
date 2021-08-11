abc8ff453095f4e9e370e743e5aa2c0dd1fa12f0
Tim Menzies <timm@ieee.org>
2021-08-11 13:53:49 -0400

    save

--- src/index.lisp
+++ src/index.lisp
@@ -5,10 +5,12 @@
 ; language in the world" and expect such comments to be taken as
 ; objective truth. I have never heard a Java, C++, C, Perl, or
 ; Python advocate make the same claim about their own language of
-; choice." 
+; choice."     
 ;
 ; --Anon
 ;
 
+; How to do anything
+; ------------------
 (defun lisp () print (eval (read)))
 
