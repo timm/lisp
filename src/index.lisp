06c4b1bf5a5e372f3e10f1ab2402f72a41c81936
Tim Menzies <timm@ieee.org>
2021-08-10 23:41:50 -0400

    save

--- src/index.lisp
+++ src/index.lisp
@@ -1,8 +1,8 @@
-  ; "I have heard more than one LISP advocate state such subjective
-  ; comments as, "LISP is the most powerful and elegant programming
-  ; language in the world" and expect such comments to be taken as
-  ; objective truth. I have never heard a Java, C++, C, Perl, or
-  ; Python advocate make the same claim about their own language of
-  ; choice."
-  ;
-  ; - A guy on Slashdot. What theory fits this data?
+; vim: ts=2 sw=2 et:
+
+; "I have heard more than one LISP advocate state such subjective
+; comments as, "LISP is the most powerful and elegant programming
+; language in the world" and expect such comments to be taken as
+; objective truth. I have never heard a Java, C++, C, Perl, or
+; Python advocate make the same claim about their own language of
+; choice." --Anon
