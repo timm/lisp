69fd0f798329eea2f3c5cd69a31bfbc081dd4d1b
Tim Menzies <timm@ieee.org>
2021-08-11 21:20:58 -0400

    save

--- src/index.lisp
+++ src/index.lisp
@@ -3,17 +3,16 @@
 ; How to...
 ; ---------
 (defun ...do-anything () (print (eval (read)))) ;
-; "I have heard more than one LISP advocate state such subjective
+; - "I have heard more than one LISP advocate state such subjective
 ; comments as, "LISP is the most powerful and elegant programming
 ; language in the world" and expect such comments to be taken as
 ; objective truth. I have never heard a Java, C++, C, Perl, or
 ; Python advocate make the same claim about their own language of
-; choice."       
+; choice."           
 ; -- Anon
-;  
-; "As computers have grown more powerful, the new languages being
+; - "As computers have grown more powerful, the new languages being
 ; eveloped have been moving steadily toward the Lisp model. A popular
 ; recipe for new programming languages in the past 20 years has been
 ; to take the C model of computing and add to it, piecemeal, parts
-; taken from Lisp."
+; taken from Lisp."    
 ; -- Paul Graham
