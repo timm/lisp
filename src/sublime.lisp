47c6ecfa9279f78089869bb99f92d6986cd90485
Tim Menzies <timm@users.noreply.github.com>
2022-02-16 21:52:24 -0500

    as

--- src/sublime.lisp
+++ src/sublime.lisp
@@ -18,31 +18,23 @@ Lets have some fun."
 
 ;;;; lib
 ;;; tricks
-(defmacro ? (p x &rest xs) (if (null xs) `(getf ,p ,x) `(? (getf ,p ,x) ,@xs)))
+(defmacro ?  (p x &rest xs) (if (null xs) `(getf ,p ,x) `(? (getf ,p ,x) ,@xs)))
+(defun args  () sb-ext:*posix-argv*)
+(defun slots (x xs) (mapcar (lambda (s) (cons s (slot-value x s))) xs))
+(defun stop  (&optional (status 0)) (sb-ext:exit :code status))
 
-(defmethod char1 ((x symbol) c) (char1 (symbol-name x) c))
-(defmethod char1 ((x string) c) (equal (elt x 0) c))
-
-;;; system
-(defun args () sb-ext:*posix-argv*)
-(defun stop (&optional (status 0)) (sb-ext:exit :code status))
-
-;;; structs
-(defstruct thing)
-(defmethod print-object ((x thing) out)
-  (labels ((pair (z) (let ((v (slot-value x z))) (if v `(,z ,v) z))))
-    (print-object (cons (class-name (class-of x)) (mapcar #'pair (show x))) out)))
-
-(defmacro defthing (x &rest slots &aux (id (gensym)))
-  (labels ((hidep (z) (char1 z #\_))
-           (name  (z) (if (consp z) (car z) z)))
+(defmacro defthing (x &rest slots &aux (id (gensym)) (it (gensym)))
+  "Defines structs with uniq ids `_id` and a constuctor `(%make-x)`
+   and a print method that hides privates slots (those starting with `_`)."
+  (labels ((hidep (z) (equal (char (symbol-name z) 0) #\_))
+           (name  (z) (if (consp z) (car z) z))
+           (names ()  (remove-if #'hidep (mapcar #'name slots)))
+           (%make ()  (intern (format nil "%MAKE-~a" (symbol-name x)))))
     `(let ((,id 0))
-       (defstruct
-           (,x (:include thing)
-               (:constructor ,(intern (format nil "%MAKE-~a" (symbol-name x)))))
-         (_id (incf ,id)) ,@slots)
-       (defmethod show ((x ,x)) ',(remove-if #'hidep (mapcar #'name slots))))))
-
+       (defstruct (,x  (:constructor ,(%make))) (_id (incf ,id)) ,@slots)
+       (defmethod print-object ((,it ,x) out)
+         (print-object (cons ',x (slots ,it ',(names))) out)))))
+  
 (defthing num n mu m3 sd)
 (defthing cols all x  klass y)
 (defthing sample rows (cols (%make-cols)))
@@ -63,9 +55,12 @@ Lets have some fun."
       (cons (item (subseq s n pos)) (str2items s (1+ pos)))
       (list (item (subseq s n)))))
 
-(defun file2items (file &optional (fn #'print))
-  (with-open-file (s file)
-    (loop (funcall fn (str2items (or (read-line s nil) (return-from file2items)))))))
+(defun %csv (file &optional (fn 'print))
+  (with-open-file (str file)
+    (loop (funcall fn (s2cells (or (read-line str nil) (return-from %csv)))))))
+
+(defmacro with-csv ((lst file &optional out) &body body)
+  `(progn (%with-csv ,file (lambda (,lst) ,@body)) ,out))
 
 ;(defun file2sample (file &aux ((s (make-sample))))
 ;;;; lib
@@ -76,6 +71,6 @@ Lets have some fun."
           do (rotatef (elt tmp (random i)) (elt tmp (1- i))))
     (coerce tmp 'list)))
 
-(print (sample-_id (%make-sample)))
+(print  (%make-sample :rows 12))
 (print (sample-_id (%make-sample)))
 (print (sample-_id (%make-sample)))
