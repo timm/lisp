f60c6674e4f29941c01a02d54074edeca6e1e242
Tim Menzies <timm@ieee.org>
2022-06-15 22:21:06 -0400

    save

--- src/espy/lines.lisp
+++ src/espy/lines.lisp
@@ -1,5 +1,4 @@
 (defun trims (x) (string-trim '(#\Space #\Tab #\Newline)))
-(defun flag? (x) (and (> (length x) 2) (equal "--" (subseq 0 2)) (subseq x 2 0)))
 
 (defun 2lst (s &optional (c #\,) (n 0)  &aux (pos (position c s :start n)))
   "Divide string `s` on character `c`."
@@ -19,11 +18,13 @@
 
      ggg,ccc")
 
-(defmacro has (x a &optional (n  1))
+(defmacro has (x a &optional (init  0))
   "A counter, implemented as an association list."
   `(cdr (or (assoc ,x ,a :test #'equal)
-            (car (setf ,a (cons (cons ,x 0) ,a))))))
+            (car (setf ,a (cons (cons ,x ,init) ,a))))))
 
-(dolist (lines (strings *s* #\newline))
-   (aif (flag? (trims (first lines)))
-     (has it (trims (car (last lines))))))
+(defun the0 (s &aux out)
+  (labels ((? (x) (and (> (length x) 2) (equal "--" (subseq 0 2)) (subseq x 2 0)))
+           (! (lines ((aif (? (trims (first line)))
+                        (push (cons it (trims (car (last line))))))))))
+    (dolist (line (strings s #\newline) out) (! line))))
