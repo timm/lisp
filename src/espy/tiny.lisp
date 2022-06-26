8ae499f93ebd37c3f6ec52ce052178ff6ed719f8
Tim Menzies <timm@ieee.org>
2022-06-26 06:18:20 -0400

    save

--- src/espy/tiny.lisp
+++ src/espy/tiny.lisp
@@ -1,27 +1,36 @@
 (defstruct row cells used)
-(defstruct (cols (%constructor %make-cols))  all x y name)
+(defstruct cols all x y name)
 (defstruct rows kept cols)
 
+; ~horthand macro for lambda.
 (defmacro λ (args &rest rest) 
-  "shorthand macro for lambda"
   `(lambda ,args ,@rest))
 
+; Recursive struct accessors; e.g. `(? s address street number)`."
 (defmacro ? (s x &rest xs)
-  "Recursive struct accessors; e.g. `(? s address street number)`."
   (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
 
-(defun reads (file f)
-  "iterate `f` over all items in `file`"
-  (with-open-file (s file) 
-    (labels ((reads1 (x) (when x (funcall f x) (reads1 (read s nil)))))
-      (reads1 (read s nil)))))
+(defmacro aif (test yes &optional no) 
+  "anaphoric 'if' (writes to 'it')"
+  `(let ((it ,test)) (if it ,yes ,no)))
 
-(reads "../../data/auto93.lisp" (λ (x) (print x)))
 
-(defun %make-cols (row &aux (i (%make-cols)))
-  (labels ((col (word)
-     
-    (setf (? i all) (mapcar #'col row)))
+(defun charn ((x symbol)) (charn (symbol-name x)))
+(defun charn ((x string)) (char x (1- (length x)))) 
+
+(defun char0 ((x symbol)) (char0 (symbol-name x)))
+(defun char0 ((x string)) (char x 0))
+
+(defun goalp  (x) (member (charn x) '(#\! #\- #\+)))
+(defun klassp (x) (eql   (charn x) #\!))
+(defun nump   (x) (uppercase-p   (char0 x)))
+
+; iterate `f` over all items in `file`
+(defun reads (s file)
+  (with-open-file (s file) (labels ((hop () (jump  (read s nil)))
+                                    (jump (x) (when x (funcall f x) (hop))))
+                             (hop))))
+
 (defmethod add ((c cols) (r row))
   (dolist (slot '(x y) r)
     (dolist (col (slot-vaue c slot)) (add col (elt row (? col at))))))
@@ -33,3 +42,5 @@
   (if (? i cols)
       (push (mapcar #'add (? i cols) r) (? i kept))
       (setf (? i cols) (make-cols row))))
+
+  
