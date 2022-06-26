d56d89929480abdc890e1ba22b993ea8dfdfc2e0
Tim Menzies <timm@ieee.org>
2022-06-26 11:29:54 -0400

    save

--- src/espy/tiny.lisp
+++ src/espy/tiny.lisp
@@ -13,22 +13,16 @@
 (defstruct (num (:include col)) (kept (make-few)))
 (defstruct (sym (:include col)) kept)
                   
-#| shorthand macro for lambda.
-|#
-(defmacro λ (args &rest rest) `(lambda ,args ,@rest))
-
 ; Recursive struct accessors; e.g. `(? s address street number)`."
 (defmacro ? (s x &rest xs)
   (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
 
-; anaphoric 'if' (writes to 'it')"
-(defmacro aif (test yes &optional no) `(let ((it ,test)) (if it ,yes ,no)))
-
 ; iterate `f` over all items in `file`
 (defun reads (file f)
-  (with-open-file (s file) (labels ((hop  ()  (jump (read s nil)))
-                                    (jump (x) (when x (funcall f x) (hop))))
-                             (hop))))
+  (with-open-file (s file) 
+    (labels ((there ()  (here (read s nil)))
+             (here  (x) (when x (funcall f x) (there))))
+      (there))))
 
 ; ## Cols                 
 (defmethod complete ((c cols))
