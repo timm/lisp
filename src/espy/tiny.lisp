92e5000b79a4dd725c51b6faf95920ceb81da04a
Tim Menzies <timm@ieee.org>
2022-06-26 13:44:41 -0400

    save

--- src/espy/tiny.lisp
+++ src/espy/tiny.lisp
@@ -1,21 +1,80 @@
-;  ._    _.  ._ _    _    _ 
-;  | |  (_|  | | |  (/_  _> 
-;                           
-(defvar *config* '(keep 256))
-(defmacro ?? (x) `(getf *config* ',x))
+#| _               
+  | |_  (_)  _ _    _  _ 
+  |  _| | | | ' \  | || |
+   \__| |_| |_||_|  \_, |
+                     |__/  
 
+(c) 2022, Tim Menzies, opensource.org/licenses/BSD-2-Clause.
+
+   _   ._   _|_  o   _   ._    _ 
+  (_)  |_)   |_  |  (_)  | |  _> 
+       |                         
+
+First we need some options. |#
+(defvar *options 
+  '((keep  256    "-K"  "items to keep            ")
+    (k     1      "-k"  "nb low attributes classes")
+    (m     2      "-n"  "nb low frequency classes ")
+    (seed  10019  "-s"  "random number seed       ")))
+
+#|._ _    _.   _  ._   _    _ 
+  | | |  (_|  (_  |   (_)  _>  
+
+Then we need some macros to handle some common short-cuts. |#
+
+;; (?? x:atom):atom ; return an option
+(defmacro ?? (x) `(second (assoc ',x *options)))
+
+;; (? x:struct &rest slots:[atom]):atom ; nested slot access
+(defmacro ? (s x &rest xs)
+  (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
+
+;; (aif test yes no) ; anaphoric `if` (remembering test results in `it`)
+(defmacro aif (test yes &optional no) 
+  `(let ((it ,test)) (if it ,yes ,no)))
+
+;   _  _|_  ._        _  _|_   _ 
+;  _>   |_  |   |_|  (_   |_  _> 
+;
+; ROWs keeps 1 record in "cell" and  sets a "used" flag if we access the "y" vals.
 (defstruct row cells used)
-(defstruct cols all x y names)
+; ROWS holds many records in "rows"; summarized in "cols".
 (defstruct rows rows cols)
+; COLS summarize the goal and independent columns in "x" and "y".
+(defstruct cols all x y names)
 
 (defstruct col (n 0) (at 0) (txt "") (w 1) )
 (defstruct (few (:include col)) kept ok (max (?? keep)))
 (defstruct (num (:include col)) (kept (make-few)))
 (defstruct (sym (:include col)) kept)
                   
+(defun thing (x &aux (y (string-trim '(#\Space #\Tab #\Newline) x)))
+  (cond ((string= y "?")     "?")
+        ((string= y "true")  t)
+        ((string= y "false") nil)
+        (t (let ((z (ignore-errors (read-from-string y))))
+             (if (numberp z) z y)))))
+
+(defun args () #+clisp *args* #+sbcl *posix-argv*)
+
+(defun cli (lst)
+  (aif (member (third lst) (args) :test 'equal) 
+    (setf (second lst)
+          (cond ((equal (second lst) t)   nil)
+                ((equal (second lst) nil) t)
+                (t (thing (second it)))))))
+
+(defvar *seed* (?? seed))
+(defun randi (&optional (n 1)) (floor (* n (/ (randf 1000.0) 1000))))
+(defun randf (&optional (n 1.0)) 
+  (setf *seed* (mod (* 16807.0d0 *seed*) 2147483647.0d0))
+  (* n (- 1.0d0 (/ *seed* 2147483647.0d0))))
+
+(mapc 'cli *options)
+
+(print *options)
 ; Recursive struct accessors; e.g. `(? s address street number)`."
-(defmacro ? (s x &rest xs)
-  (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
+
 
 ; iterate `f` over all items in `file`
 (defun reads (file f)
@@ -34,7 +93,7 @@
              (skipp  (x) (eql    (charn x) #\:))
              (klassp (x) (eql    (charn x) #\!))
              (what   (x) (if (uppercase-p (char0 x)) 'make-num 'make-sym))
-             (make1  (txt &aux (col (funcall (whatp txt)) :at (incf at) :txt txt))
+             (make1  (txt &aux (col (funcall (what txt)) :at (incf at) :txt txt))
                        (if (eql #\- (charn txt)) (setf (? col w) -1))
                        (unless (skipp txt)
                          (if (klassp txt) (setf (? c klass) col))
@@ -61,5 +120,3 @@
   (if (? i cols)
       (push (mapcar #'add (? i cols) r) (? i kept))
       (setf (? i cols) (make-cols row))))
-
-  
