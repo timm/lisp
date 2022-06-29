3bc6a8efff940808268fe2b830fc43eb2eea7fce
Tim Menzies <timm@ieee.org>
2022-06-28 22:01:37 -0400

    as

--- src/espy/tiny.lisp
+++ src/espy/tiny.lisp
@@ -1,60 +1,67 @@
 ; vim: ts=2 sw=2 et :
+(defpackage :tiny (:use :cl))
+(in-package :tiny)
+
 (defvar *about* 
   '("TINY (c) 2022, Tim Menzies" 
     "Multi-objective semi-supervised XAI, in a few 100 lines."))
 
 (defvar *options* 
-   `((help  nil    "-h"  "show help")
-     (keep  256    "-K"  "items to keep")
-     (k     1      "-k"  "nb low attributes classes")
-     (m     2      "-n"  "nb low frequency classes")
-     (seed  10019  "-s"  "random number seed")))
+   `((file   "-f"   "help file                "  "../../data/auto93.lisp")
+     (help   "-h"   "show help                "  nil)
+     (keep   "-K"   "items to keep            "  256)
+     (k      "-k"   "nb low attributes classes"  1)
+     (m      "-n"   "nb low frequency classes "  2)
+     (seed   "-s"   "random number seed       "  10019)))
 
-;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; 
+;;;; ## Library
+;;; ### Macros
+; (aif if then else) :; anaphoric `if` (remembers results of `if` in `it`)
 (defmacro aif (test yes &optional no) `(let ((it ,test)) (if it ,yes ,no)))
-(defun thing (x &aux (y (string-trim '(#\Space #\Tab #\Newline) x)))
-  (cond ((string= y "?")     "?")
-        ((string= y "true")  t)
-        ((string= y "false") nil)
-        (t (let ((z (ignore-errors (read-from-string y))))
-             (if (numberp z) z y)))))
-
-(defun cli (about lst)  
-  (dolist (four lst)
-    (let* ((args #+clisp *args* #+sbcl *posix-argv*))
-      (aif (member (third four) args :test 'equal)
-        (setf (second four) (cond ((equal (second four) t)   nil)
-                                  ((equal (second four) nil) t)
-                                  (t (thing (second it))))))))
-  (when (second (assoc 'help lst))
-    (format t "~&~%~{~a~%~}~%OPTIONS:~%" about)
-    (dolist (a lst) 
-      (format t "  ~a  ~7a ~a~%" (elt a 2) (elt a 1) (elt a 3)))))
-
-;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; 
-; ## Macro Short-cuts
-; Some macros to handle some common short-cuts. 
 
 ; (?? x:atom):atom :; return an option
-(defmacro ?? (x) `(second (assoc ',x *options*)))
+(defmacro ?? (x) `(fourth (assoc ',x *options*)))
 
 ; (? x:struct &rest slots:[atom]):atom :; nested slot access
 (defmacro ? (s x &rest xs)
   (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
 
-; (aif test yes no) :; anaphoric `if` (remembering test results in `it`)
-
 ; A counter, implemented as an association list.
-(defmacro inca (x a &optional (n  1))
-  `(incf (cdr (or (assoc ,x ,a :test #'equal)
-                  (car (setf ,a (cons (cons ,x 0) ,a)))))
-         ,n))
+(defmacro counts (x a)
+  `(cdr (or (assoc ,x ,a :test #'equal)
+            (car (setf ,a (cons (cons ,x 0) ,a))))))
 
+;;; ### Misc
+; (str->thing x:str):atom :; 
+(defun str->thing (x &aux (y (string-trim '(#\Space #\Tab #\Newline) x)))
+  (if (string= y "?")     
+    "?"
+    (let ((z (ignore-errors (read-from-string y))))
+      (if (numberp z) z y))))
+
+(defun cli (about lst)  
+  (dolist (four lst)
+    (let* ((args #+clisp ext:*args* #+sbcl sb-ext:*posix-argv*))
+      (aif (member (second four) args :test 'equal)
+        (setf (fourth four) (cond ((equal (fourth four) t)   nil)
+                                  ((equal (fourth four) nil) t)
+                                  (t (str->thing (second it))))))))
+  (when (fourth (assoc 'help lst))
+    (format t "~&~%~{~a~%~}~%OPTIONS:~%" about)
+    (dolist (a lst) 
+      (format t "  ~a  ~a  ~a ~%" (elt a 1) (elt a 2) (elt a 3)))))
+
+;; ### Random number generation.
 (defvar *seed* (?? seed))
 (defun randi (&optional (n 1)) (floor (* n (/ (randf 1000.0) 1000))))
 (defun randf (&optional (n 1.0)) 
   (setf *seed* (mod (* 16807.0d0 *seed*) 2147483647.0d0))
   (* n (- 1.0d0 (/ *seed* 2147483647.0d0))))
+
+; first and  last characters of string
+(defun chars (x) (if (stringp x) x (symbol-name x)))
+(defun charn (x &aux (y (chars x))) (char y (1- (length y))))
+(defun char0 (x &aux (y (chars x))) (char y 0))
 
 ; iterate `f` over all items in `file`
 (defun reads (file f)
@@ -62,10 +69,6 @@
     (labels ((there ()  (here (read s nil)))
              (here  (x) (when x (funcall f x) (there))))
       (there))))
-
-(defun chars (x) (if (stringp x) x (symbol-name x)))
-(defun charn (x) (char x (1- (length x))))
-(defun char0 (x) (char x 0))
 
 ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; ;;;;;;;;; 
 ; ROWs keeps 1 record in "cell" and  sets "used" if we access the "y" vals.
@@ -76,31 +79,39 @@
 (defstruct (cols (:constructor %make-cols)) all x y names)
 
 (defstruct col (n 0) (at 0) (txt "") (w 1) )
-(defstruct (few (:include col)) kept ok (n 0) (max (?? keep)))
 (defstruct (num (:include col)) (kept (make-few)))
 (defstruct (sym (:include col)) kept)
-                  
-(defmethod add ((n num) x &optional (inc 1))
+(defstruct (few (:include col)) 
+  (kept (make-array 2 :fill-pointer 0 :adjustable t)) 
+  (max (?? keep))
+  ok)
+
+(defmethod add ((self num) x)
   (unless (eql '? x)
-    (loop repeat inc
-      do (incf (? c inc))
-         (add (? n kept) x))))
+    (incf (? self n))
+    (add (? self kept) x)))
 
-(defmethod add ((s some) (x number))
-  (incf (? s n))
-  (let ((size (length (? s kept))))
-    (cond ((< size  (? s max))
-           (push-vector-extend x (? s kept))
-           (setf (? s ok) nil))
-          ((< (randf) (/ (? s n) (? s max)))
-           (setf (elt (? s kept) (randi size) ) x)
-           (setf (? s ok) nil)))))
+(defmethod add ((self sym) x)
+  (unless (eql '? x)
+    (incf (? self n))
+    (incf (counts  (? self kept) x))))
 
-(defmethod kept (s) (? s kept))
-(defmethod kept ((s some))
-  (if (not (? s ok)) (sort (? s kept) '<))
-  (setf (? s ok) t)
-  (? s kept))
+(defmethod add ((self few) (x number))
+  (incf (? self n))
+  (let ((size (length (? self kept))))
+    (cond ((< size  (? self max))
+           (vector-push-extend x (? self kept))
+           (setf (? self ok) nil))
+          ((< (randf) (/ (? self n) (? self max)))
+           (setf (elt (? self kept) (randi size) ) x)
+           (setf (? self ok) nil)))))
+
+(defmethod kept (self) (? self kept))
+(defmethod kept ((self few))
+  (unless  (? self ok) (setf (? self kept) 
+                             (sort (? self kept) #'<)))
+  (setf (? self ok) t)
+  (? self kept))
 
 ; ## Cols                 
 (defun make-cols (names &aux (cols (%make-cols :names (mapcar 'chars names))))
@@ -111,23 +122,25 @@
                      (make-sym :at (incf at) :txt txt))))
         (push col (? cols all))
         (setf (? cols w) (if (eql #\- (charn txt)) -1 1))
-        (unless (eql (charn x) $\:)
-          (if (eql #\:  (charn txt)) (setf (? cols klass) col))
+        (unless (eql #\: (charn txt))
+          (if (eql   #\: (charn txt)) (setf (? cols klass) col))
           (if (member (charn txt) '(#\! #\- #\+)) 
             (push col (? cols y))
             (push col (? cols x))))))))
 
-(defmethod add ((c cols) (r row))
+(defmethod add ((self cols) (r row))
   (dolist (slot '(x y) r)
-    (dolist (col (slot-value c slot)) 
-      (add col (elt (? row cells) (? col at))))))
+    (dolist (col (slot-value self slot)) 
+      (add col (elt (? r cells) (? col at))))))
                    
 ; ## rows
-(defmethod add ((i rows) (r cons)) (add i (make-row :cells r)))
-(defmethod add ((rs rows) (r row)) i
-  (if  (? rs cols) 
-    (push (add (? i cols) r) (? rs rows))
-    (seff (? rs cols) (make-cols r))))
+(defmethod add ((self rows) (r cons)) (add self (make-row :cells r)))
+(defmethod add ((self rows) (r row)) 
+  (if (? self cols) 
+    (push (add (? self cols) r) (? self rows))
+    (setf (? self cols) (make-cols r))))
 
 (cli *about* *options*)
-(print *options*)
+
+(print (let ((n (make-num)))
+         (dotimes (i 100 (kept (? n kept))) (add n i))))
