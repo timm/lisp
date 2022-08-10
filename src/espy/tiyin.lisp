57c7a45fe3e9142c661897221524c74b9e6d2c62
Tim Menzies <timm@ieee.org>
2022-08-10 00:25:58 -0400

    save

--- src/espy/tiyin.lisp
+++ src/espy/tiyin.lisp
@@ -28,11 +28,27 @@
           (t (it (second it))))))
 
 (setf *opt* (mapcar (lambda (x) (cons (car x) (apply 'cli (cdr x))))  *opt*))
+  
+(defun chars (x) (if (symbolp x) (symbol-name x) x))
+(defun char0 (x) (char (chars x) 0))
+(defun charn (x) (let ((y (chars x))) (char y (1- (length y)))))
 
-(defstruct (cols (:constructor %make-cols)) all x y names)
+(defstruct (sym  (:constructor %make-sym))  (txt "") (at 0) kept)
+(defstruct (num  (:constructor %make-num))  (txt "") (at 0) kept ok (w 1))
+(defstruct (cols (:constructor %make-cols)) names all x y klass)
 
-(defun make-cols (lst &aux (self (%make-cols :names lst)))
-   (setf (cols-all self) (mapcar (lambda (x) (make-col x (incf at)))))
+(defun make-sym (s n) (%make-sym :txt s :at n))
+(defun make-num (s n) (%make-num :txt s :at n :w (if (equal #\- (charn s)) -1 1)))
 
+(defun make-cols (lst)
+  (let (all x y kl (pos -1))
+    (dolist (s lst (%make-cols :names lst :all (reverse all) :x x :y y :klass kl))
+      (let* ((what (if (equal #\$ (char0 s)) 'make-num 'make-sym))
+             (col  (funcall what s (incf pos))))
+        (push col all)
+        (unless (equal #\x (charn s))
+          (print 1)
+          (if (member (charn s) '(#\! #\- #\+)) (push y col) (push x col))
+          (if (equal #\! (charn s)) (setf kl col)))))))
 
-
+(print (make-cols '($aa bb! cc+)))
