8aed4ef3a79191ba134303d27eeb8a5b43875e1b
Tim Menzies <timm@ieee.org>
2022-08-10 08:41:44 -0400

    save

--- src/espy/tiyin.lisp
+++ src/espy/tiyin.lisp
@@ -38,28 +38,32 @@
   (with-open-file (s file)
     (loop (funcall fun (or (read s nil) (return-from reads))))))
 
-(defstruct pretty)
-(defmacro defthing (x &body slots) 
-   (let ((slots1 (mapcar (lambda (x) (if (consp x) (car x) x)) slots)))
-      `(defstruct (,x (:include pretty)
-                      (:constructor ,(intern (format nil "%MAKE-~a" x)))) 
-         (_slots ,slots1) ,@slots)))
+(defmacro ? (s x &rest xs)
+ (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
 
-; (defmethod print-object ((x pretty) str)
-;   (let ((want (remove-if (lambda (z) (eq #\_ (char0 x)))  (? x _slots)  (lambda `(cons ,x 
-;
-; (defmacro ? (s x &rest xs)
-;   (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
-;
+(defun slots (obj slots)
+  (mapcar (lambda (y) (format nil ":~(~a~) ~a" y (slot-value obj y))) slots))
+                            
+(defstruct pretty)
+(defmacro defthing (x &body body) 
+  (let ((show (remove-if (lambda (x) (eq #\_ (char (symbol-name x) 0))) 
+                         (mapcar (lambda (x) (if (consp x) (car x) x)) body))))
+    `(progn
+       (defstruct (,x (:include pretty) 
+                      (:constructor ,(intern (format nil "%MAKE-~a" x)))) ,@body)
+       (defmethod print-object ((self ,x) str)
+         (format str "~a" (cons ',x (slots self ',show)))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defthing sym  (txt "") (at 0) kept)
 (defthing num  (txt "") (at 0) kept ok (w 1))
 (defthing cols names all x y klass)
 (defthing data rows about)
-(defthing row   rows about)
+(defthing row  cells _about)
 
 (defun make-sym (s n) (%make-sym :txt s :at n))
 (defun make-num (s n) (%make-num :txt s :at n :w (if (equal #\- (charn s)) -1 1)))
+(defun make-row (about l)   (%make-row :cells l :_about about))
 
 (defun make-cols (lst)
   (let (all x y kl (pos -1))
@@ -71,14 +75,14 @@
           (if (member (charn s) '(#\! #\- #\+)) (push  col y) (push  col x))
           (if (eq #\! (charn s)) (setf kl col)))))))
 
-(defun make-data (names &optional src (i (%make-data :about make-cols names)))
+(defun make-data (names &optional src (i (%make-data :about (make-cols names))))
   (if (stringp src)
     (reads src (lambda (row) (add i row)))
     (dolist (row src) (add i row)))
  i)
 
-  
-; (print (make-data '($aa bb!~ cc+)))
+(print (make-row 12 '(1 2 3 4)))
+(print (make-data '($aa bb!~ cc+)))
 ;
 ; (defmethod clone ((d data) &optional src) (make-data (? d about names) src))
 ;(reads "../../data/auto93.lisp" 'print)
