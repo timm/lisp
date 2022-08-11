84155fcf73e854bced0a2f6bbcbee2a3732a5267
Tim Menzies <timm@users.noreply.github.com>
2022-08-11 16:37:12 -0400

    save

--- src/tiny/lib.lisp
+++ src/tiny/lib.lisp
@@ -41,10 +41,10 @@
 ;;; maths
 ; Random number control (since reseeding in LISP is... strange).
 (defvar *seed* 10013)
-(defun randi (&optional (n 1)) (floor (* n (/ (randf 1000000000.0) 1000000000))))
 (defun randf (&optional (n 1.0)) 
   (setf *seed* (mod (* 16807.0d0 *seed*) 2147483647.0d0))
   (* n (- 1.0d0 (/ *seed* 2147483647.0d0))))
+(defun randi (&optional (n 1)) (floor (* n (/ (randf 1000000000.0) 1000000000))))
 
 ;;; settings
 ; Update `default` from command line (if it contains `flag` or `key`).
--- src/tiny/sym.lisp
+++ src/tiny/sym.lisp
@@ -1,4 +1,3 @@
 (defstruct+ sym  (txt "") (at 0) kept)
 
-(defun make-sym (s n) (%make-sym :txt s :at n))
-
+(defun make-sym (s n) (%make-sym :txt s :at n))
