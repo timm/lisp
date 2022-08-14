24178855d0af23fe6336071a224cd871e5efdc9d
Tim Menzies <timm@ieee.org>
2022-08-14 00:21:36 -0400

    save

--- src/tiny/eg.lisp
+++ src/tiny/eg.lisp
@@ -2,13 +2,6 @@
 (load "tiny")
 (in-package :tiny)
 
-;(print (make-row 12 '(1 2 3 4)))
-; (print (make-about '("$aa" "bb!~" "cc+")))
-; (print (! my 'seed))
-; (dotimes (i 20) (print (randi  200)))
-; ; (defmethod clone ((d data) &optional src) (make-data (? d about names) src))
-; ;(reads "../../data/auto93.lisp" 'print)
-
 (defdemo my () "show options" (pprint my) t)
 
 (defdemo div () "num divs" 
