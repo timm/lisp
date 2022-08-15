ab5d89ebcd9a374a0f97f92fe26cb3b4a4e37075
Tim Menzies <timm@users.noreply.github.com>
2022-08-15 12:12:17 -0400

    save

--- src/tiny/row/rows.lisp
+++ src/tiny/row/rows.lisp
@@ -3,27 +3,26 @@
                  cols)  ; summaries of all the columns
 
 (defun make-rows (&optional src (i (%make-rows)))
-  (labels 
-    ((handle (x) (if (? i cols) 
-                   (push (add i x) rows) 
-                   (setf (? i cols) (make-cols x)))))
+  (labels ((ensure-cols-exists (x) (if (? i cols) 
+                                     (push (add i x) rows) 
+                                     (setf (? i cols) (make-cols x)))))
     (if (stringp src)
-      (with-lines src #'handle)
-      (mapcar #'handle src))
+      (with-lines src #'ensure-cols-exists)
+      (mapcar #'ensure-cols-exists src))
     i))
 
 (defmethod clone ((i rows) &optional src) 
-  (make-rows (? i cols names) src))
+  (make-rows (cons (? i cols names)) src))
 
 (defmethod add ((i rows) (lst cons)) (add i (make-row i lst)))
 (defmethod add ((i rows) (row1 row))
   (dolist (cols `(,(? i cols x) ,(? i cols y)) row1)
-    (dolist (col cols row1)
+    (dolist (col cols)
       (add col (elt (? row1 cells) (? col at))))))
 
 (defmethod dist ((self rows) (row1 row) (row2 row))
   (let ((d 0) (n 0))
     (dolist (col (? self cols x) (float (expt (/ d n) (! my p))))
-      (incf n)
+      (incf n):vsp
       (incf d (dist col (elt (? row1 cells) (? col at)) 
                         (elt (? row2 cells) (? col at)))))))
