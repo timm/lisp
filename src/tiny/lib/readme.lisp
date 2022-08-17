1e49e0e8af0c156d15d2d7ea9a83498328f2301b
Tim Menzies <timm@ieee.org>
2022-08-17 05:44:36 -0400

    save

--- src/tiny/lib/readme.lisp
+++ src/tiny/lib/readme.lisp
@@ -30,10 +30,7 @@
       (reads f 
         (lambda (x)
           (when (and (defp x) (docp x) (not (secret x)))
-            (format s "|`~(~a~)` | `~(~a~)` |~a <details><summary>xx</summary>adsaas
-asdads
-                    asd
-                    asda</details> |~%"
+            (format s "|`~(~a~)` | `~(~a~)` |~a |~%"
                     (second x) (or (third x) "") 
                     (substitute #\SPACE #\NEWLINE (string (fourth x))))))))))
 
