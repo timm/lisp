3b3bfa6e934cce7ad471f579022c8ee9128ad904
Tim Menzies <timm@ieee.org>
2022-12-20 00:18:51 -0500

    save

--- runr.lisp
+++ runr.lisp
@@ -3,12 +3,16 @@
 
 (defun l() (load "runr"))
 
-(defun trim (x)  
-  "kill leading,trailing whitespace"
+(defmacro ? (s x &rest xs) 
+  "recursive slot-value access"
+  (if (null xs) `(slot-value ,s ',x) `(? (slot-value ,s ',x) ,@xs)))
+
+(defun trim (x) 
+ "kill leading,trailing whitespace"
   (string-trim '(#\Space #\Tab #\Newline) x))
 
 (defun thing (x &aux (y (trim x)))
-  "coerce `x` into a number or string or t or nil or #\?"
+ "coerce `x` into a number or string or t or nil or #\?"
   (if (equal y "?") #\?
     (if (equal y "t") t
       (if (equal y "nil") nil
@@ -22,6 +26,22 @@
           (if there (words str char filter (1+ there))))))
 
 (defun with-lines (file fun)
-  "Call `fun` for each line in `file`."
+  "Call `fun` for each line in `file`"
   (with-open-file (s file)
     (loop (funcall fun (or (read-line s nil) (return))))))
+
+(defun ako ((s string) kind)
+  "given a column header, comment on its the propertoes of that column"
+  (if (upper-case-p (char s 0)) 
+    'num
+    (first (member (char s (1- (length s))) 
+                   (cdr (assoc kind '((ignore #\X) (klass #\!) (less #\-)
+                                                   (more #\+) (goal #\+ #\- #\!))))))))
+
+(defstruct num (at 0) (txt "") (n 0) (mu 0) (m2 0) (lo 1E31) (hi -1E21))
+
+(defun num! (&optional (at 0) (txt ""))
+  (make-num :at at :txt txt :w (if (ako txt 'less) -1 1)))
+
+(defmethod add ((self num) x)
+  )
