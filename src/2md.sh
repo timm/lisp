9b1b99cc391126f8435d6984bf27957414db0297
Tim Menzies <timm@users.noreply.github.com>
2023-02-14 18:27:36 -0500

    save

--- src/2md.sh
+++ src/2md.sh
@@ -1,4 +1,17 @@
-BEGIN { print "<html><head></head><body>" }
+BEGIN { print "<html><head>"
+       print "<style type=\"text/css\">"
+        print  "body {"
+         print  "  font-family: Optima, Segoe, "Segoe UI", Candara, Calibri, Arial, sans-serif;"
+         print  "  line-height: 1.4;"
+         print  "  max-width: 45rem;"
+         print  "  padding: 2rem;"
+         print  "  margin: auto;"
+         print  "    background: #edeef2;"
+         print  "}"
+         print  "pre { font: small; border: 1px dotted lightblue; background: #E0E0E0;}"
+         print  "img.right200 { width: 200px; border:1px solid #999; padding:3px; margin:5px; float:right; }"      
+         print "</style>" 
+         print "</head><body>" }
 END   { print "</body></html>" }
 
 /vi: set/            { next }
@@ -7,3 +20,8 @@ END   { print "</body></html>" }
                      { gsub(/(#\||\|#)/,"")
                        gsub(/^[;]*/,"")
                        print }
+
+
+
+
+  
