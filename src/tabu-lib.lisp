bac1b03cbfdb9457b6aea0c0f9faee678299ed33
Tim Menzies <timm@users.noreply.github.com>
2023-02-18 20:36:28 -0500

    save

--- src/tabu-lib.lisp
+++ src/tabu-lib.lisp
@@ -1,3 +1,13 @@
+; vi: set ts=2 sw=2 sts=2 et :
+;  __                 __                           ___              __        
+; /\ \__             /\ \                         /\_ \      __    /\ \       
+; \ \ ,_\     __     \ \ \____   __  __           \//\ \    /\_\   \ \ \____  
+;  \ \ \/   /'__`\    \ \ '__`\ /\ \/\ \  _______   \ \ \   \/\ \   \ \ '__`\ 
+;   \ \ \_ /\ \L\.\_   \ \ \L\ \\ \ \_\ \/\______\   \_\ \_  \ \ \   \ \ \L\ \
+;    \ \__\\ \__/.\_\   \ \_,__/ \ \____/\/______/   /\____\  \ \_\   \ \_,__/
+;     \/__/ \/__/\/_/    \/___/   \/___/             \/____/   \/_/    \/___/ 
+
+;## Macros
 (defmacro ? (x &optional (lst '*settings*)) 
   "alist accessor, defaults to searching `*settings*`"
   `(cdr (assoc ',x ,lst)))
@@ -6,32 +16,22 @@
   "used to test on a result that is also needed by `then`"
   `(let ((it ,test)) (if it ,then ,else)))
 
-
 (defmacro freq (x lst &optional (init 0))      
   "frequency counts for small group of symbols (say, less than 50)"
   `(cdr (or (assoc ,x ,lst :test #'equal) 
             (car (setf ,lst (cons (cons ,x ,init) ,lst))))))
-
+;-------------------------------------------------------------------------------
+;## Sys
 (defun args () 
   "accessing command-line flats"
   #+clisp ext:*args*  
   #+sbcl sb-ext:*posix-argv*)
-
-(defun thing (s &aux (s1 (trim s)))
-  "coerce `s` into a number or string or t or nil or #\?"
-  (cond ((equal s1 "?") #\?)
-        ((equal s1 "t") t)
-        ((equal s1 "nil") nil)
-        (t (let ((n (read-from-string s1 nil nil))) 
-             (if (numberp n) n s1)))))
-
-(defun got (s c &optional (n 0))
-  "Does `s` hold `c` at position `n` (negative `n` means 'from end of string')"
-  (if (and (stringp s) (> (length s) 0))
-    (if (< n 0) 
-      (got s c (+ (length s) n))
-      (and (>= n 0) (< n (length s)) (eql c (char s n))))))
-
+;-------------------------------------------------------------------------------
+;##  Lists
+(defun per (seq &optional (p .5))
+  (elt seq (floor (* (min .999999 (max 0 p)) (length seq)))))
+;-------------------------------------------------------------------------------
+;## Strings
 (defun trim (s) 
   "kill whitespace at start, at end"
   (string-trim '(#\Space #\Tab #\Newline) s))
@@ -53,12 +53,22 @@
 (defun words (s) 
   "divide a string on space"
   (split s #\Space #'trim))
+;-------------------------------------------------------------------------------
+;## Strings to Things
+(defun thing (s &aux (s1 (trim s)))
+  "coerce `s` into a number or string or t or nil or #\?"
+  (cond ((equal s1 "?") #\?)
+        ((equal s1 "t") t)
+        ((equal s1 "nil") nil)
+        (t (let ((n (read-from-string s1 nil nil))) 
+             (if (numberp n) n s1)))))
 
 (defun with-file (file fun &optional (filter #'split))
   "call `fun` for each line in `file`"
   (with-open-file (s file) 
     (loop (funcall fun (funcall filter (or (read-line s nil) (return)))))))
-
+;-------------------------------------------------------------------------------
+;## Settings
 (defun settings (s &optional args)
   "for lines like '  -Key Flag ..... Default', return `(KEY . DEFAULT)`"
   (loop 
@@ -75,8 +85,3 @@
           ((eql b4 nil) t)
           (t (thing (second it))))
     b4))
-
-(defun per (seq &optional (p .5))
-  (elt seq (floor (* (min .999999 (max 0 p)) (length seq)))))
-
-
--- src/tabu.lisp
+++ src/tabu.lisp
@@ -1,4 +1,12 @@
 ; vi: set ts=2 sw=2 sts=2 et :
+;  __                 __                 
+; /\ \__             /\ \                
+; \ \ ,_\     __     \ \ \____   __  __  
+;  \ \ \/   /'__`\    \ \ '__`\ /\ \/\ \ 
+;   \ \ \_ /\ \L\.\_   \ \ \L\ \\ \ \_\ \
+;    \ \__\\ \__/.\_\   \ \_,__/ \ \____/
+;     \/__/ \/__/\/_/    \/___/   \/___/ 
+
 (defpackage :tabu (:use :cl))
 (in-package :tabu)
 (load "tabu-lib")
@@ -13,60 +21,46 @@ OPTIONS:
   -s   seed   random numbe seed  = 10013")
 
 (defvar *settings* (settings *help* (args)))
-;----------------------------------------------------------
+;-------------------------------------------------------------------------------
+;## Structs
+(defstruct data 
+  "stores `rows`, sumamrized sin `cols`"
+  rows cols)
 
+(defstruct cols 
+  "stores everything in `all`, independent/dependent things in `x`,`y`"
+  all x y names)
+
+(defstruct num 
+  "sumamrizes a stream of numbers"
+  (at 0) (txt "") (n 0) (w 1) ; w=1,-1 means "maximize", "minimize"
+  (hi most-negative-fixnum) 
+  (lo most-positive-fixnum)
+  ok   ; true if `has` is currently sorted.
+  (has ; holds ?max items. if full, new items replace any old
+     (make-array 5 5 :fill-pointer 0 :adjustable t)))
+
+(defstruct sym 
+  "sumamrizes a stream of symbols"
+  (at 0) (txt "") (n 0) (w 1) has (most 0) mode)
+;-------------------------------------------------------------------------------
+;## Column Header Language
 (defun isNum   (s) (and (> (length s) 0) (upper-case-p (char s 0))))
 (defun isGoal  (s) (or (isKlass s) (isLess s) (isMore s)))
 (defun isKlass (s) (got s #\! -1))
 (defun isLess  (s) (got s #\- -1))
 (defun isMore  (s) (got s #\+ -1))
 (defun isIgnored (s) (got s #\X -1))
-
-(defstruct data rows cols)
-
-(defstruct cols all x y names)
-
-(defstruct sym 
-  (at 0) (txt "") (n 0) (w 1) has (most 0) mode)
-
-(defstruct num 
-  (at 0) (txt "") (n 0) (w 1) 
-  (hi most-negative-fixnum) 
-  (lo most-positive-fixnum)
-  ok
-  (has (make-array 5 :fill-pointer 0 :adjustable t)))
-
-(defmethod add ((self sym) x)
-  (with-slots (has n mode most) self
-   (unless (eql x #\?)
-     (incf n)
-     (incf (freq x has))
-     (if (> (freq x has) most) (setf most (freq x has) mode x)))))
-
-(defmethod add ((self num) x)
-  (with-slots (has ok n lo hi) self
-    (unless (eql x #\?)
-      (incf n)
-      (setf lo (min lo x)
-            hi (max hi x))
-      (cond ((< (length has) (? max)) 
-             (setf ok nil) 
-             (vector-push-extend x has))
-            ((<= (rand) (/ (? max) n)) 
-             (setf ok nil) 
-             (setf (aref has (rint (length has))) x))))))
-
-(defun holds (num)
-  (unless (num-ok num) (sort (num-has num) #'<))
-  (setf (num-ok num) t)
-  (num-has num))
-
+;-------------------------------------------------------------------------------
+;## Create
 (defun str->col (&key (txt "") (at 0))
+  "column name to num or sysm"
   (funcall  (if (isNum txt) #'make-num #'make-sym) 
             :at at :txt txt
             :w   (if (isLess txt) -1 1)))
 
 (defun lst->cols (lst &aux (self (make-cols :names lst)) (n -1))
+  "column names to cols, then added to `all` and (maybe) `x`, `y`, and `klass`"
   (labels 
     ((worker (txt &aux (col (str->col :txt txt :at (incf n)))) 
              (with-slots (all x y klass) self
@@ -85,15 +79,47 @@ OPTIONS:
           ((data-p src)   (row  (cols-names (data-cols src)))))
     (mapc #'row rows)
     self))
-
+;-------------------------------------------------------------------------------
+;## Add
 (defmethod add ((self data) lst)
+  "updates `rows` and `cols`"
   (aif (data-cols self)
     (push (add it lst)  (data-rows self))
     (setf (data-cols self) (lst->cols lst))))
 
 (defmethod add ((self cols) lst)
+  "update nums and syms"
   (dolist (tmp `(,(cols-x self) ,(cols-y self)) lst)
     (dolist (col tmp)
       (add col (elt lst (slot-value col 'at))))))
+
+(defmethod add ((self sym) x)
+  "update frequency counts (in `has`) and `most` and `mode`"
+  (with-slots (has n mode most) self
+   (unless (eql x #\?)
+     (incf n)
+     (incf (freq x has))
+     (if (> (freq x has) most) (setf most (freq x has) mode x)))))
+
+(defmethod add ((self num) x)
+  "updates `lo`, `hi`, `has` and `ok`"
+  (with-slots (has ok n lo hi) self
+    (unless (eql x #\?)
+      (incf n)
+      (setf lo (min lo x)
+            hi (max hi x))
+      (cond ((< (length has) (? max)) 
+             (setf ok nil) 
+             (vector-push-extend x has))
+            ((<= (rand) (/ (? max) n)) 
+             (setf ok nil) 
+             (setf (aref has (rint (length has))) x))))))
+;-------------------------------------------------------------------------------
+;## Query
+(defun holds (num)
+  "returns `has`, sorted"
+  (unless (num-ok num) (sort (num-has num) #'<))
+  (setf (num-ok num) t)
+  (num-has num))
 
 (print  (holds (second (cols-x  (data-cols (src->data (? file)))))))
