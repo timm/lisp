9944ab5e7716676c94c422c3691fe803a4f7e65c
timm <timm@ieee.org>
2025-01-05 01:21:05 -0500

    push

--- src/k.lisp
+++ src/k.lisp
@@ -33,6 +33,11 @@
 #+sbcl (declaim (sb-ext:muffle-conditions cl:style-warning))
 
 ;### Macros
+(defmacro aif (test yes &optional no)
+  "Anaphoric if. Condition results available in `it`."
+  `(let ((it ,test))
+     (if it ,yes ,no)))
+
 (defmacro o (x f &rest fs)
   "Nested access to slots."
   (if fs
@@ -82,11 +87,12 @@
 ;#### Num (isa _Col_, has _mu_, _sd_, _lo_ and _hi_ bounds)
 (defstruct (num (:include col) (:constructor %make-num))
   "`Num`s summarize numeric columns."
-  (mu 0) (m2 0) (sd 0) (lo 1E32) (hi -1E32) (goal 1))
+  (mu 0) (m2 0) (sd 0) (lo 1E32) (hi -1E32) (goal 1)
+  rank meta ; used by stats
+  )
 
 (defun make-num (&key (txt "") (pos 0))
   "Constructor. For `nums`."
-
   (%make-num :txt txt :pos pos :goal (if (eql #\- (chr txt -1)) 0 1)))
 
 ;### Cols (factory for making _Cols_ from list of _names_)
@@ -109,6 +115,11 @@
 
 ;## Methods
 ;### Update
+(defmethod add ((self col) (seq sequence))
+  "Run `add` for all items  in `seq`."
+  (map nil (lambda (item) (add self item)) seq)
+  seq)
+
 (defmethod add ((self data) row)
   "Keep the row, update the `cols` summaries."
   (if $cols
@@ -116,17 +127,18 @@
       (setf $cols (make-cols row))))
 
 (defmethod add ((self cols) row)
+  "Update `Cols.all` with `row`." 
   (mapcar (lambda (col) (add col (at col row))) $all)
   row)
-
-(defmethod add ((self col) x)
+  
+(defmethod add ((self col) x &key (n 1))
   "For non-empty cells, add `x`. Always return `x`."
   (unless (eql x '?)
-    (incf $n)
-    (add1 self x))
+    (incf $n n)
+    (add1 self x) n)
   x)
 
-(defmethod add1 ((self num) x)
+(defmethod add1 ((self num) x _)
   "Update numeric summaries with `x`."
   (let ((d (- x $mu)))
     (incf $mu (/ d $n))
@@ -135,57 +147,116 @@
           $lo (min x $lo)
           $hi (max x $hi))))
 
-(defmethod add1 ((self sym) x)
+(defmethod add1 ((self sym) x n)
   "Update symbolic summaries with `x`."
-  (let ((new (incf (has $counts x))))
+  (let ((new (incf (has $counts) n)))
     (if (> new $most)
       (setf $mode x
             $most new))))
 
+;### Merge
+(defmethod merge ((i sym) (j sym) (eps 20))
+  "Merge `i`,`j` if they are too small or if too complex."
+  (let ((k (make-sym)))
+    (loop :for (x . n) :in (o i counts) :do (add k x n))
+    (loop :for (x . n) :in (o j counts) :do (add k x n))
+    (if (or (< (o i n) eps)
+            (< (o j n) eps)
+            (<= (ent k) (/ (+ (* (o i n) (ent i))
+                              (* (o j n) (ent j)))
+                           (o k n))))
+        k)))
+
+(defun merges (lst xtra &aux out pairs)
+  "Run `merge` over all items in `lst`."
+  (dolist (one lst)
+    (if out
+        (aif (merge one (car out) xtra)
+             (setf (car out) it)
+             (push one out))
+        (setf out (list one)))
+    (push `(,one ,(length out)) pairs))
+  (loop :for (one n) :in pairs :do
+    (setf (o one meta) (elt out n)
+          (o one meta rank) (code-char (+ (char-code #\a) n))))
+  out)
+ 
 ;### Query
 (defmethod at ((self col) row)
   "Access a column in a row."
   (elt row $pos))
 
+(defmethod entropy ((self sym))
+  "Return diversity of a sym's counts."
+  (labels ((f (n) (* (/ n $n) (log (/ n $n) 2))))
+    (loop :for (_ . n) :in $counts :sum (f n))))
+
 (defmethod norm ((self num) x)
   "Normalizes x 0..1."
   (if (eql x '?) x (/ (- x $lo) (- $hi $lo 1E-32))))
 
+(defmethod delta ((i num) (j num))
+  "Return mean difference, normalized by sd."
+  (let ((n1  (+ 1E-32 (o i n)))  (n2 (+ 1E-32 (o j n)))
+        (mu1 (o i mu)) (mu2 (o j mu))
+        (sd1 (o i sd)) (sd2 (o j sd)))
+    (/ (abs (- mu1 mu2))
+       (sqrt (+ (/ (* sd1 sd1) n1)
+                (/ (* sd2 sd2) n2))))))
+
 (defmethod ydist ((self data) row)
+  "Over y columns, return distance to goals."
   (minkowski
    (o $cols y)
    (lambda (col) (- (o col goal) (norm col (at col row))))))
 
 (defmethod xdist ((self data) row1 row2)
+  "Over x columns, return distance between items."
   (minkowski
    (o $cols x) 
    (lambda (col) (dist col (norm col (at col row1)) (norm col (at col row2))))))
 
-(defmethod around ((self data) row1 &optional (rows $rows))
-  (sort rows #'< :key (lambda (row2) (xdist self row1 row2))))
+(defmethod kprune ((self data) &keys (k 25) (n 32) (rows (many $rows n)))
+  "k-means++ centroid generaion."
+  (let ((centroids `(,(any rows))))
+    (dotimes (k1 (1- k) centroids)
+      (push (_kprune self centroids rows) centroids))))
 
-(defmethod kth ((self data) row1 &optional (k 5) (rows $rows))
-  (subseq (around self row1 rows) k))
+(defun _kprune (data centroids rows &aux pairs (all 0))
+  "Returns one new k-means++ centroid."
+  (dolist (row rows)
+    (labels ((dist (r) (xdist data r row)))
+      (let ((near (car (sort centroids #'< :key #'dist))))
+        (incf all (car (push `(,(expt (dist near) 2) ,row) pairs))))))
+  (setf all (randf all))
+  (or (loop :for (d row) :in pairs :if (<= (decf all d) 0) :return row)
+      (second (car pairs))))
 
-(defmethod kprune ((self data) k &keys (samples 32))
-  (let ((out `(,(any $rows))))
-    (dotimes (k1 (1- k) out)
-      (push (_kprune self out (many $rows samples))
-            out))))
-
-(defun _kprune (data out rows)
-  (let ((n (make-num)))
-    (labels (aroun22
-             (mapcar 
-               #'(ambda (r) let ((far (car (around  data one rows))))
-               `(,(xdist data far one) far))  
-               data one 1 row
-                         (sort (lambda (rorows
-                (first (sort out #'< :key (lambda (r2)
-                                            `(, (add n (expt (xdist data r1 r2) 2)) r2))))))
-                  
 ;## Functions
-;### Numeric trics
+;### Stats
+(defun cliffs (xs ys &key (delta 0.197) &aux (n 1E-32) (lt 0) (gt 0))
+  "Non-parametric effect size."
+  (dolist (x xs) 
+    (dolist (y ys (<= (/ (abs (- gt lt)) n) delta))
+      (incf n)
+      (if (> y z) (incf gt))
+      (if (< y z) (incf lt)))))
+
+(defun bootstrap (y0 z0 &keys (b 512) (conf 0.05))
+  "Non-parametric significance test."
+  (labels ((nums (lst) (add (make-num) lst)))
+    (let* ((x    (nums (nums y0) z0))
+           (y    (nums y0))
+           (z    (nums z0))
+           (obs  (delta y z))
+           (yhat (mapcar (lambda (y1) (- y1 (o y mu) (o x mu))) y0))
+           (zhat (mapcar (lambda (z1) (- z1 (o z mu) (o x mu))) z0))
+           (n    0))      
+      (dotimes (_ b (>= (/ n b) conf))
+        (if (>= (delta (nums (many yhat)) (nums (many zhat))) obs)
+            (incf n))))))
+
+;### Numerics
 (defun inca (a x)
   "Ensure `a` has a key for `x`, add one to that count."
   (incf (cdr (or (assoc x a :test #'equal)
@@ -197,17 +268,8 @@
            (length lst))
         (/ 1 (? pp))))
 
-;### Lisp Tricks
-;#### Random
-(defun pick (seq f &aux (all 0))
-  (labels ((pairs (x &aux (fx (funcall x)))
-             (incf all fx)
-             `(,fx ,x )))
-    (let* ((seq (sort (mapcar seq #'pairs) #'> :key #'car))
-           (r (randf all)))
-      (or (loop :for (fx x) :in seq :if (<= (decf r fx) 0) :return x)
-          (car seq)))))
-                                          
+
+;#### Randoms                                        
 (defun any (seq)
   "return a random item from seq"
   (elt seq (floor (randf (length seq)))))
@@ -235,7 +297,7 @@
   (setf *seed* (mod (* 16807.0d0 *seed*) 2147483647.0d0))
   (* n (- 1.0d0 (/ *seed* 2147483647.0d0))))
 
-;### String tricks
+;### Strings
 (defun chr (s n )
   "Return nth character from `s`. Negative `n` denote indexes from back." 
   (let ((s (if (stringp s) s (symbol-name s))))
@@ -255,7 +317,7 @@
     (cons (thing (subseq s here there))
           (if there (things s sep (1+ there))))))
 
-;### File I/O
+;### Files
 (defun with-csv (&optional file (fun #'print) end)
   "call `fun` on all lines in `file`, after running lines through `filter`"
   (with-open-file (s (or file *standard-input*))
@@ -264,7 +326,6 @@
 
 ;##  Start-up
 ;### Actions
-  
 (defun eg-s(s)
   (setf *seed* (setf (? seed) s)))
 
@@ -284,7 +345,7 @@
          (format t "~,2f ~a~%" (ydist self row) row)))))
 
 ;### Start-up Control
-(loop :for (flag arg) :on (args) :by #'cdr
+(loop :for (flag arg) :on (args) :by #'cdr(< (o i n) eps)
       :do  (let ((com (intern (format nil "EG~:@(~a~)" flag))))
               (if (fboundp com)
                  (funcall com (if arg (thing arg))))))
