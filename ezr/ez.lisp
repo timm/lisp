597bf317babbfa1b5b3810e07d46fc426326f295
timm <timm@ieee.org>
2026-03-06 02:20:36 -0500

    as

--- ezr/ez.lisp
+++ ezr/ez.lisp
@@ -2,37 +2,39 @@
 (defparameter *the* '(:k 1 :m 2 :p 2 :decs 2 :seed 1))
 
 (defmacro ? (x) `(getf *the* ,(intern (string x) "KEYWORD")))
+
+(defmacro o (x &rest slots) 
+   (if (null slots) x `(o (slot-value ,x ',(car slots)) ,@(cdr slots))))
+
 (set-macro-character #\$
   (lambda (s c) (declare (ignore c)) `(slot-value i ',(read s t nil t))))
-(defmacro aif (test then) `(let ((it ,test)) (when it ,then)))
 
 ;── structs ──────────────────────────────────────────────────────────
-(defstruct col  (at 0) (txt "") goal (n 0d0))
-(defstruct (num (:include col)) (mu 0d0) (m2 0d0))
-(defstruct (sym (:include col)) (has (make-hash-table :test 'equal)))
-(defstruct cols names all x y)
 (defstruct data (n 0) rows cols mids)
+(defstruct num  (n 0) (at 0) (txt "") (mu 0) (m2 0) (goal 0))
+(defstruct sym  (n 0) (at 0) (txt "") (has (make-hash-table :test 'equal)))
+(defstruct cols names all x y)
 
 ;── lib ──────────────────────────────────────────────────────────────
-(defun unk       (v)     (equal v "?"))
-(defun last-char (s)     (char s (1- (length s))))
-(defun cell      (c row) (elt row (col-at c)))
-(defun say (x)
+(defun unk  (v) (equal v "?"))
+(defun cell (c row) (elt row (col-at c)))
+(defmethod say (x) 
   (if (floatp x) (format nil "~,vf" (? decs) x) (princ-to-string x)))
 
-(defun cast (s &aux (s (string-trim " " s)))
-  (cond ((string-equal s "true")  1)
-        ((string-equal s "false") 0)
-        (t (let ((n (read-from-string s nil nil)))
-             (if (numberp n) n s)))))
+(defun cast (s &aux (s (string-trim " " s)) (*read-eval* nil)) 
+  (labels ((fn (s n) (case n
+                       (true  1)
+                       (false 0)
+                       (t (if (numberp n) n s)))))
+    (fn s (read-from-string s nil nil))))
 
 (defun atoms (ch str &aux (b (position ch str)))
-  (cons (cast (string-trim " " (subseq str 0 (or b (length str)))))
-          (when b (atoms ch (subseq str (1+ b))))))
+  (cons (cast (subseq str 0 (or b (length str))))
+        (if b (atoms ch (subseq str (1+ b))))))
 
-(defun mapcsv (&optional (fun #'print) file end)
+(defun mapcsv (fun file &optional end &key (sep #\,))
   (with-open-file (s (or file *standard-input*))
-      (loop (funcall fun (atoms (or (read-line s nil) (return end)))))))
+      (loop (funcall fun (atoms sep (or (read-line s nil) (return end)))))))
 
 (defun align (m &aux (ss (mapcar (lambda (r) (mapcar #'say r)) m)))
   (let ((ws (loop for i below (length (car ss))
@@ -44,43 +46,51 @@
 (defun eachn (lst &optional (n 30))
   (loop for x in lst for i from 0 when (zerop (mod i n)) collect x))
 
+(defun end (x &aux (s (if (stringp x) x (col-txt x))))
+  (char s (1- (length s))))
+
 ;── structs ──────────────────────────────────────────────────────────
-(defun col! (at txt &aux (g (char/= (last-char txt) #\-)))
-  (if (upper-case-p (char txt 0)) (make-num :at at :txt txt :goal g)
-                                  (make-sym :at at :txt txt :goal g)))
+(defun col! (&optional (at 0) (txt " "))
+  (if (upper-case-p (char txt 0))
+    (make-num :at at :txt txt :goal (if (char= (end txt) #\-) 0 1))
+    (make-sym :at at :txt txt)))
 
-(defun cols! (names &aux (at -1))
-  (labels ((end   (c) (last-char (col-txt c)))
-           (make! (s) (col! (incf at) s)))
-    (let ((all (mapcar #'make! names)))
-      (make-cols :names names :all all
-        :x (remove-if     (lambda (c) (find (end c) "-+!X")) all)
-        :y (remove-if-not (lambda (c) (find (end c) "-+!"))  all)))))
+(defun cols! (names)
+  (let ((all (loop for s in names for at from 0 collect (col! at s))))
+    (make-cols :names names :all all
+               :x (remove-if     (lambda (c) (find (end c) "-+!X")) all)
+               :y (remove-if-not (lambda (c) (find (end c) "-+!"))  all))))
 
-(defun data! (file &aux (d (make-data)))
-  (csv file (lambda (r) (add d r))) d)
+(defun data! (src &aux (data (make-data)))
+  (let ((fn (if (stringp src) #'mapcsv #'mapc)))
+    (funcall fn (lambda (row) add src) src)
+    data))
 
 ;── update ───────────────────────────────────────────────────────────
-(defmethod add ((i sym) v &optional (w 1d0))
-  (unless (unk v) (incf $n w) (incf (gethash v $has 0d0) w)))
+(defun sub (i v) (add i v -1))
 
-(defmethod add ((i num) v &optional (w 1d0))
-  (unless (unk v)
+(defmethod add (i x &optional (w 1))
+  (unless (equal x "?")
     (incf $n w)
-    (if (<= $n 0) (setf $mu 0d0 $m2 0d0)
-        (let ((d (- v $mu)))
-          (incf $mu (* w (/ d $n))) (incf $m2 (* w d (- v $mu)))))))  )
+    (add1 i x w))
+  x)
 
-(defmethod add ((i data) row &optional (w 1d0))
-  (if $cols (addRow i row w) (setf $cols (cols! row))) row)
+(defmethod add1 ((i sym) x w)
+  (incf (gethash x $has 0) w))
 
-(defun addRow (i row w)
-  (setf $mids nil $n (+ $n w))
+(defmethod add1 ((i num) x w &aux (d (- x $mu)))
+  (incf $mu (/ (* w d) $n))
+  (incf $m2 (* w d (- x $mu))))
+
+(defmethod add1 ((i data) row w)
+  (if $cols (add1Row2Data i row w) (setf $cols (cols! row))))
+  
+(defun add1Row2Data (i row w)
+  (setf $mids nil)
   (dolist (c (cols-all $cols)) (add c (cell c row) w))
-  (if (plusp w) (push row $rows)
+  (if (> w 0)
+      (push row $rows)
       (setf $rows (remove row $rows :test #'equal :count 1))))
-
-(defun sub (i v) (add i v -1d0))
 
 ;── query ────────────────────────────────────────────────────────────
 (defmethod mid ((i num)) $mu)
@@ -91,40 +101,38 @@
 
 (defun mids (i) (or $mids (setf $mids (mapcar #'mid (cols-all $cols)))))
 
-(defmethod spread ((i num))
-  (if (< $n 2) 0d0 (sqrt (/ (max 0d0 $m2) (1- $n)))))
+(defmethod spread ((i num)) (if (< $n 2) 0 (sqrt (/ (max 0 $m2) (1- $n)))))
 
-(defmethod spread ((i sym) &aux (e 0d0))
+(defmethod spread ((i sym) &aux (e 0))
   (maphash (lambda (_ v)
-    (when (plusp v) (let ((p (/ v $n))) (decf e (* p (log p 2)))))) $has)
+    (if (> v 0) (let ((p (/ v $n))) (decf e (* p (log p 2)))))) $has)
   e)
 
-(defun z (i v) (max -3d0 (min 3d0 (/ (- v $mu) (+ (spread i) 1d-30)))))
+(defun z (i v) (max -3 (min 3 (/ (- v $mu) (+ (spread i) 1e-30)))))
 
 (defmethod norm ((i sym) v) v)
 (defmethod norm ((i num) v)
-  (if (unk v) v (/ 1d0 (+ 1d0 (exp (* -1.7d0 (z i v)))))))
+  (if (unk v) v (/ 1 (+ 1 (exp (* -1.7 (z i v)))))))
 
 ;── distance ─────────────────────────────────────────────────────────
-(defun minkowski (vals &aux (n 0) (s 0d0) (p (? p)))
-  (dolist (x vals (if (zerop n) 0d0 (expt (/ s n) (/ 1d0 p))))
+(defun minkowski (vals &aux (n 0) (s 0) (p (? p)))
+  (dolist (x vals (if (zerop n) 0 (expt (/ s n) (/ 1 p))))
     (incf n) (incf s (expt x p))))
 
 (defun disty (i r)
-  (minkowski (mapcar (lambda (y &aux (v (norm y (cell y r))))
-                       (- (if (unk v) 0d0 v) (if (col-goal y) 1d0 0d0)))
-                     (cols-y $cols))))
+  (labels ((fn (y) (abs (- (norm y (cell y r)) (o y goal)))))
+    (minkowski (mapcar #'fn (o i cols y)))))
 
 (defun distx (i r1 r2)
-  (labels ((aha (i u v)
-             (cond ((and (unk u) (unk v)) 1d0)
-                   ((sym-p i) (if (equal u v) 0d0 1d0))
-                   (t (let ((nu (norm i u)) (nv (norm i v)))
-                        (when (unk nu) (setf nu (if (> nv 0.5) 0d0 1d0)))
-                        (when (unk nv) (setf nv (if (> nu 0.5) 0d0 1d0)))
-                        (abs (- nu nv)))))))
-    (minkowski (mapcar (lambda (x) (aha x (cell x r1) (cell x r2)))
-                       (cols-x $cols)))))
+  (labels ((aha (c) (let ((u (cell c r1))
+                          (v (cell c r2)))
+                      (cond ((and (unk u) (unk v)) 1)
+                            ((sym-p c) (if (equal u v) 0 1))
+                            (t (let ((nu (norm c u)) (nv (norm c v)))
+                                 (if (unk nu) (setf nu (if (> nv 0.5) 0 1)))
+                                 (if (unk nv) (setf nv (if (> nu 0.5) 0 1)))
+                                 (abs (- nu nv))))))))
+    (minkowski (mapcar #'aha (o i cols x)))))
 
 (defun nearest  (d r rows) (car      (order d r rows)))
 (defun furthest (d r rows) (car (last (order d r rows))))
@@ -132,21 +140,21 @@
   (sort (copy-list rows) #'< :key (lambda (r2) (distx d r r2))))
 
 (defmethod nearby ((i sym) v &aux
-    (n (* (random 1d0) (loop for v being the hash-values of $has sum v))))
+    (n (* (random 1) (loop for v being the hash-values of $has sum v))))
   (maphash (lambda (k v) (when (<= (decf n v) 0) (return-from nearby k)))
            $has))
 
 (defmethod nearby ((i num) v)
   (+ (if (unk v) $mu v)
-     (* 2 (spread i) (- (loop repeat 3 sum (random 1d0)) 1.5d0))))
+     (* 2 (spread i) (- (loop repeat 3 sum (random 1)) 1.5))))
 
 ;── bayes ────────────────────────────────────────────────────────────
-(defmethod like ((i num) v prior &aux (var (+ (expt (spread i) 2) 1d-30)))
-  (* (/ 1d0 (sqrt (* 2 pi var)))
+(defmethod like ((i num) v prior &aux (var (+ (expt (spread i) 2) 1e-30)))
+  (* (/ 1 (sqrt (* 2 pi var)))
      (exp (/ (- (expt (- v $mu) 2)) (* 2 var)))))
 
 (defmethod like ((i sym) v prior)
-  (max 1d-32 (/ (+ (gethash v $has 0d0) (* (? k) prior)) (+ $n (? k)))))
+  (max 1e-32 (/ (+ (gethash v $has 0) (* (? k) prior)) (+ $n (? k)))))
 
 (defun prior (i n-all n-h) (/ (+ $n (? m)) (+ n-all (* (? m) n-h))))
 
@@ -154,7 +162,7 @@
   (+ (log p)
      (loop for x in (cols-x $cols) for v = (cell x row)
            unless (unk v) sum (let ((l (like x v p)))
-                                (if (plusp l) (log l) 0d0)))))
+                                (if (plusp l) (log l) 0)))))
 
 ;── demos ────────────────────────────────────────────────────────────
 (defun eg_the (&optional f) (declare (ignore f)) (format t "~a~%" *the*))
@@ -183,7 +191,7 @@
 
 (defun eg_bayes (f &aux (d (data! f)) (n (data-n d)))
   (dolist (r (eachn (data-rows d)))
-    (format t "~a~%" (say (/ (round (* (likes d r n 1) 100d0)) 100d0)))))
+    (format t "~a~%" (say (/ (round (* (likes d r n 1) 100)) 100)))))
 
 ;── main ─────────────────────────────────────────────────────────────
 (defun cli (args)
