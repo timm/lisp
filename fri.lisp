0584a4473ad4cb8f67793be92983731754291936
timm <timm@ieee.org>
2026-03-20 23:07:51 -0400

    as

--- fri.lisp
+++ fri.lisp
@@ -1,21 +1,76 @@
+; vim: set ft=lisp ts=2 sw=2 et lispwords+=with-slots :
 (defparameter *the* 
   '(:p 2 :file "auto93.csv"))
 
-(defstruct sym   (at 0) (txt " ") (n 0) has)
-(defstruct (num  (:constructor %num)) (at 0) (txt " ") (n 0) (mu 0) )
-(defstruct (name /////////////////////////////////////////////////////                                       (:constructor %cols) names x y all)
-(defstruct data rows (cols (make-cols)) mids)
+(defmacro ? (x) `(getf *the* ,(->key x)))
 
-(defun make-num (&rest args ) 
-  (let ((new (apply #'%num args)))
-    (setf (num-goal new) (if (eq #\- (at -1 (num-txt new))) 0 1)))
-    new)
+(defstruct sym (at 0) (txt " ") (n 0) has)
+(defstruct num (at 0) (txt " ") (n 0) (mu 0) )
+(defstruct cols names x y all)
+(defstruct data rows (cols (make-cols)) _mids)
+
+(defun sym! (&rest arg) (apply #'make-sym args))
+
+(defun num! (&rest args &aux (new (apply #'make-num args)))
+  (setf (num-goal new) (if (eq #\- (at -1 (num-txt new))) 0 1))
+  new)
+
+(defun cols! (names &aux (new (make-cols :names names)))
+  (with-slots (all x y) new
+    (dolist (s names)
+      (let ((new (col! (ch s -1) (length all))))
+        (push new all)
+        (unless (eql (ch s -1)  #\X)
+          (if (find (ch s -1) "!-+") (push new y) (push new x)))))
+    (setf all (reverse all))
+    new))
+
+(defun data! (rows (&aux (it (make-data :cols (cols! (pop rows))))))
+  (dolist (row rows it) (add it row)))
+
+;------------------------------------------------------------------------------
+(defun add (x v (w 1))
+  (unless (eql #\? x)
+    (incf (slot-vale x 'n))
+    (add1 x v w))
+  x)
+
+(defmethod add1 ((d data) row w)
+  (with-slots (rows cols _mids) d
+    (setf _mids nil)
+    (push row rows)
+    (mapcar (lambda (col v) (add col v w)) all row)))
+
+(defun _sym+ (i v inc) 
+  (incf (cdr (or (assoc v $has :test #'equal)
+                 (car (setf $has (cons (cons v 0) $has))))) inc))
+
+(defun _cols+ (i v inc) 
+  (mapcar (=> (x col) (add col x inc)) row $all))
+
+(defun _num+ (i v inc) 
+  (setf $lo (min v $lo)
+        $hi (max v $hi))
+  (if (and (< inc 0) (< $n 2))
+    (setf $mu 0 $m2 0 $sd 0 $n 0)
+    (let* ((d (- v $mu)))
+      (incf $mu (* inc (/ d $n)))
+      (incf $m2 (* inc (* d (- v $mu))))
+      (setf $sd (if (< $n 2) 0 (sqrt (/ (max 0 $m2) (1- $n))))))))
+
+(defun _data+  (i v inc zap)
+  (cond ((not $cols) (setf $cols (new (make-cols) :names row)))
+        ((> inc 0) (push row $rows)
+                   (add $cols row inc))
+        (t         (if zap (setf $rows (remove row $rows :test #'equal)))
+                   (sub $cols v))))
+
+
+(defmethod add1 ((n num) n)
+  (let ((d 
 
 (defun ->key (x) 
   (intern (string-upcase (string x)) :keyword))
-
-(defmacro ? (x) 
-  `(getf *the* ,(->key x)))
 
 (defun thing (s &aux (n (ignore-errors (read-from-string s))))
   (if (numberp n) n s))
@@ -35,20 +90,14 @@
     for key = (->key (subseq k 1))
     do (if (member key *the*) (setf (getf *the* key) (thing v)))))
 
-(defun at (n s) 
+(defun ch (s n) 
   (char s (if (minusp n) (+ (length s) n) n)))
 
-(defun col (s at)
-  (funcall (if (upper-case-p (at -1 s)) #'make-num #'make-sym) 
-           :txt s :at at))
+(defun col! (s at)
+  (if (upper-case-p (at -1 s)) 
+    (num! :at at :txt s)
+    (sym! :at at :txt s)))
 
-(defun make-cols (names (&aux (it (%make-cols :names names))))
-   (with-slots (all x y)  it)
-      (setf all (loop for txt in names for at from 0 collect (col txt at)))
-      (let ((last (at -1 (it-txt c))))
-         (unless (eql last #\X) (push c x))
-         (when (find last "+-!") (push c y))))
-    it)
-
+  
 (print *the*)
-(print (read-csv (? file)))
+(let ((rows (read-csv (? file)))
