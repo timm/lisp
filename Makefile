ac5e77a8f24a1415303753c4d5e0e2c27906e7cd
timm <timm@ieee.org>
2026-04-28 00:48:20 -0400

    asda

--- Makefile
+++ Makefile
@@ -1,3 +1,25 @@
+SHELL    := /bin/bash
+GIT_ROOT := $(shell git rev-parse --show-toplevel 2>/dev/null)
+ETC      := $(GIT_ROOT)/etc
+
+CLS     := '\033[H\033[J'
+cRESET  := '\033[0m'
+cYELLOW := '\033[1;33m'
+
+help: ## show help
+	@awk 'BEGIN{FS=":.*##"} \
+	      /^[a-zA-Z_%\/.~$$-]+:.*##/ \
+	      {printf "  \033[36m%-20s\033[0m %s\n",$$1,$$2}' \
+	      $(MAKEFILE_LIST)
+
+sh: ## launch dev shell (banner + etc/bash.rc)
+	@-echo -e $(CLS)$(cYELLOW); figlet -W -f mini '(lisp)'; \
+	  echo -e $(cRESET)
+	@-bash --init-file $(ETC)/bash.rc -i
+
+push: ## commit with prompted msg and push
+	@read -p "Reason? " msg; git commit -am "$$msg"; git push; git status
+
 Lines?=95
 ~/tmp/%.pdf: %.lisp  Makefile ## .py ==> .pdf
 	@mkdir -p ~/tmp
---
+++ data.lisp
@@ -0,0 +1,31 @@
+#|# Hello, That's Odd
+
+The classic first programming exercise is to print "hello world".
+Which is a little dull and not very interactive.
+
+What I prefer is to  react to cahnges in the world. So I want "hello,
+that's odd" (to alert me) and "watch the world then react to is a
+dull thing to do asa Pick "hello, what's worth watching?" — keeps
+reactive theme (world changing, alert me) + double-W alliteration
+   + question stays open.
+
+## Once upon a time...
+Once upon a time, I wondered the world wondering at all I saw. There
+was a lonely number
+|#
+
+3
+
+; wiich, as I watched, changed into something else:
+
+4
+
+#| This was puzzling. Id did not know if "4" was good or bad, Nor did
+I know if I
+needed to run towards it, or to run away.
+
+So I watched for a while to learn what numbers were usual:
+|# 
+
+2 3 4 2 3 4 4  2 2 3 4 5 3 3 4 3 3  4 3 3 4 3 
+
